28a9640c850a025bdfb279d971da7e475fdf1908
16amattice <anthonymattice@gmail.com>
2017-12-13 16:52:03 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+            <title>Hexo - Freelancing Services</title>
+            
+        <meta charset="utf-8">
+<script src="js/jquery-3.2.1.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+    
+    <link rel="stylesheet" type="text/css" href="styles.css">
+<script src="js\jquery-3.2.1.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+
+
+
+
+    </head>
+
+
+<body>
+    <div id="navbar"></div>
+    
+    
+    
+    </body>
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
